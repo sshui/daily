737a6e534098cc6588aaf9950271d3d9a368396c
sshui <lamansong@gmail.com>
2016-07-16 18:57:38 +0800

    第一次提交

---
+++ ContentResolver/app/src/main/java/com/song/contentresolver/MainActivity.java
@@ -0,0 +1,46 @@
+package com.song.contentresolver;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView contactsView;
+    ArrayAdapter<String> adapter;
+    List<String> contactList =new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contactsView=(ListView)findViewById(R.id.contacts_view);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contactList);
+        contactsView.setAdapter(adapter);
+        readContacts();
+    }
+
+    private void readContacts() {
+        Cursor cursor=null;
+        try {
+            cursor=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            while (cursor.moveToNext()){
+                String displayName=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String number=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contactList.add(displayName+"\n"+number);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor!=null){
+                cursor.close();
+            }
+        }
+    }
+
+}
